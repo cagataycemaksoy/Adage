77df7276d516f425533b4af94e9161dd5ada267e
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-24 23:55:53 +0100

    phrase context added

---
+++ Adage/PhraseContext.swift
@@ -0,0 +1,14 @@
+//
+//  PhraseContext.swift
+//  Adage
+//
+//  Created by Cem Aksoy on 24.11.2025.
+//
+
+import Foundation
+
+enum PhraseContext: String, CaseIterable {
+  case daily = "Daily"
+  case business = "Business"
+  case academic = "Academic"
+}
