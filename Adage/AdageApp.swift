480cebd4e0b365e5ea8170d620c62f9d8d53708f
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-24 23:41:43 +0100

    Initial Commit

---
+++ Adage/AdageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdageApp.swift
+//  Adage
+//
+//  Created by Cem Aksoy on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AdageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
