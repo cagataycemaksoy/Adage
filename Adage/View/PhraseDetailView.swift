b7c474be26004d94b9fbebed8a13a6ce48b2bbf6
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-26 22:54:37 +0100

    detail view added

---
+++ Adage/View/PhraseDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PhraseDetailView.swift
+//  Adage
+//
+//  Created by Cem Aksoy on 26.11.2025.
+//
+
+import SwiftUI
+
+struct PhraseDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhraseDetailView()
+}
