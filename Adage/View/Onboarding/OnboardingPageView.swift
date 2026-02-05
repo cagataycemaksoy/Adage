265cb5cfc80b94fc6ca41892ef1353774f878f0f
cagataycemaksoy <cemaksoy7890@gmail.com>
2026-02-05 17:40:23 +0100

    OnboardingPageView updated

--- Adage/View/Onboarding/OnboardingPageView.swift
+++ Adage/View/Onboarding/OnboardingPageView.swift
@@ -13,6 +13,8 @@ struct OnboardingPageView: View {
   let description: String
   let lastView: Bool
   
+  let buttonAction: () -> Void
+  
     var body: some View {
       ZStack {
         LinearGradient(colors: [.color1.opacity(0.4), .color1.opacity(0.5)],
@@ -20,11 +22,45 @@ struct OnboardingPageView: View {
                        endPoint: .bottom)
         .ignoresSafeArea()
         
+        VStack {
+          Spacer()
+          
+          Text(title)
+            .font(Font.custom("Georgia", size: 38))
+            .multilineTextAlignment(.center)
+            .padding()
+          
+          Spacer()
+          
+          Image(imgString)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+          //TODO: Use GeoReader proxy for the frame height
+            .frame(height: 250)
+            .padding(.bottom, 30)
+          
+          Text(description)
+            .font(Font.custom("Georgia", size: 20))
+            .foregroundStyle(.secondary)
+            .multilineTextAlignment(.center)
+            .frame(width: 320)
+            .padding(.bottom, 30)
+          
+          Button(lastView ? "Get Started" : "Next", action: buttonAction)
+            .tint(.black)
+            .buttonStyle(.bordered)
+            .scaleEffect(1.3)
+            .padding(.vertical)
+          
+          Spacer()
+          Spacer()
+        }
+        
         
       }
     }
 }
 
 #Preview {
-    OnboardingPageView(title: "Welcome to \n Adage!", imgString: "img0", description: "Learn the language by studying real world phrases.", lastView: false)
+  OnboardingPageView(title: "Welcome to \n Adage!", imgString: "img0", description: "Learn the language by studying real world phrases.", lastView: false, buttonAction: {})
 }
